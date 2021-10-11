2e6b753cb82d7c9cd1d6b18a229886d787d1c0cc
tommysonsylverstone <tenreiro.benjamin@gmail.com>
2021-10-11 16:49:11 +0200

    modified html classes by IDs so I can use the style property in javascript. Animation will be done in javascript instead of CSS this time

--- time.js
+++ time.js
@@ -6,6 +6,11 @@ var s = d.getSeconds();
 var m = d.getMinutes();
 var h = d.getHours();
 
+var sAngle = (s * 6) - 90
+//css style properties does not work with classname
+document.getElementById("seconde").style.transform = "rotate(" + sAngle + "deg)";
+//sHand.style.transform = "rotate(" + sAngle + "deg)";
+
 (h > 12) ? h = h - 12 : h = h;
 
-console.log(h)
+console.log(sAngle)
