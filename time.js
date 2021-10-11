b7a248f942812002b0b7a0db54a4a5bd0d8f7d59
tommysonsylverstone <tenreiro.benjamin@gmail.com>
2021-10-11 17:43:05 +0200

    minute's hand is now moving along with second's hand

--- time.js
+++ time.js
@@ -7,13 +7,20 @@ var m = d.getMinutes();
 var h = d.getHours();
 
 var sAngle;
-//css style properties does not work with classname
+var mAngle;
+
 (h > 12) ? h = h - 12 : h = h;
 
-setInterval(function() {
+setInterval(function () {
     s = s + 1;
     (s > 59) ? s = 0 : s = s;
-    sAngle = (s * 6) - 90
-    document.getElementById("seconde").style.transform = "rotate(" + sAngle + "deg)";
-    console.log(sAngle)
+    sAngle = (s * 6) - 90; //seconds starting at -90°
+    document.getElementById("seconde").style.transform = "rotate(" + sAngle + "deg)"; //css style properties does not work with classname
+    if (s == 0) {
+        m = m + 1;
+        (m > 59) ? m = 0 : m = m;
+        mAngle = (m * 6) - 90;
+        document.getElementById("minute").style.transform = "rotate(" + mAngle + "deg)";
+        console.log(mAngle)
+    }
 }, 1000);
